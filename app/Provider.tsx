3bec5b6d1fdb2c0e3f035455adbaae539f1568e9
Chetan <joshic626@gmail.com>
2024-06-23 03:42:49 +0530

    dlt yarn.lock file

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,19 +1,18 @@
 "use client";
-import { useEffect, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
-import {
-  DynamicContextProvider,
-  useUserWallets,
-} from "@dynamic-labs/sdk-react-core";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 
 export function Providers(props: { children: ReactNode }) {
+  const Dynamic_KEY = process.env.DYNAMIC_API_KEY;
+
   return (
     <Provider store={store}>
       <DynamicContextProvider
         settings={{
-          environmentId: "fe350575-4c7c-4297-aff2-9a3de9e5c479",
+          environmentId: Dynamic_KEY || "",
           walletConnectors: [StarknetWalletConnectors],
         }}
       >
