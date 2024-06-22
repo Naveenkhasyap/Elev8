80e51ef0e1bdbb077a8464c85231b5c0f7a75a07
Chetan <joshic626@gmail.com>
2024-06-23 05:00:26 +0530

    elev8 video added in home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import Carts from "@/component/cart/Carts";
 import CryptoTable from "@/component/cryptoTable/CryptoTable";
 import { fetchTokens } from "@/utils/apis";
+import { elev8 } from "@/utils/Images";
 
 export default function Home() {
   const bgRef = useRef<HTMLVideoElement>(null);
@@ -33,15 +34,25 @@ export default function Home() {
   return (
     <main>
       <Header />
-
+      <div className="relative h-[75vh] w-full">
+        <video
+          ref={bgRef}
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={elev8} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="px-10">
-        <section className="flex flex-col py-15">
+        <section className="flex flex-col ">
           <div className="flex  justify-between w-full gap-5 mt-24">
             <Carts title={"🔥 Trending"} coins={treadingCoins} />
             <Carts title={"🚀 Largest Gainers"} coins={largestGainer} />
           </div>
         </section>
-
         <section className="mt-10">
           <CryptoTable tokens={tokens} />
         </section>
--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -1,2 +1,3 @@
 export const logo = "https://www.svgrepo.com/show/1320/rocket.svg";
 export const celebrate = "https://ik.imagekit.io/zt5dmrbrg/Elev8/celebrate.gif";
+export const elev8 = "https://ik.imagekit.io/zt5dmrbrg/Elev8/Elev8.mp4";
